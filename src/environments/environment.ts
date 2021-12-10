601bb894447a5bc78e31862f50add87458f8bfa7
AraiNylaz <47991166+AraiNylaz@users.noreply.github.com>
2021-12-10 12:59:55 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  api: 'http://localhost:9000',
+  api: 'https://pfe-vinci-market-frontend.herokuapp.com/',
 };
-
+//http://localhost:9000
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
