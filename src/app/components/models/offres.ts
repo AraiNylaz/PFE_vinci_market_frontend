99288fea0eb4a8f6149f821ff2b95d1f34e51856
michaelout20 <57799934+michaelout20@users.noreply.github.com>
2021-12-15 22:42:15 +0100

    vu de l offre

--- src/app/components/models/offres.ts
+++ src/app/components/models/offres.ts
@@ -1,11 +1,13 @@
 import 'reflect-metadata';
+import { User } from './user';
 export class Offre {
   idProduct?: string;
   idBuyer?: string;
   value?: Float32Array;
   message?: string;
+  buyer?: User;
 
   get display(): string {
-    return `${(this.value, ' ', this.message)}`;
+    return `prix :${(this.value, '  ', this.message)}`;
   }
 }
